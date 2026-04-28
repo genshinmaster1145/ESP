b0681152f2e1876793e21176e13a7b7bd973a50e
genshinmaster1145 <ridiculouslose@gmail.com>
2026-04-28 16:59:24 +0800

    Delete api.js

--- api.js
+++
@@ -1,6 +0,0 @@
-// functions/api.js 临时测试版
-export async function onRequest(context) {
-  return new Response("Hello from EdgeOne Functions!", {
-    headers: { "Content-Type": "text/plain" }
-  });
-}
